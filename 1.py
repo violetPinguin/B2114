f7d417d7f3ed9aad246dd1a159b60b9c466a7621
Lavrenchuk <makedonia97@gmail.com>
2025-03-30 11:31:42 +0300

    get passport program

---
+++ 1.py
@@ -0,0 +1,19 @@
+'''print("Hello world!")
+print("Hello world!"+" Helo Kitty!")
+print(2+2)
+print(" Hello world! "*10)
+print("Мені", 27 ,"рочків.")'''
+
+
+# - однорядковий коментар
+'''
+багаторядковий
+ коментар
+'''
+name = input("Enter your name:")
+print("Hello", name)
+age = int(input("Enter your age:"))
+if age >= 14:
+    print("Get your passport.")
+else:
+    print("Age up.")
